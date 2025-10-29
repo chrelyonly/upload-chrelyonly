d7dba1cb285d5e83766177c75382620a591a48bf
chrelyonly <1172576293@qq.com>
2025-10-29 17:45:42 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  base: "/image-hosting-ui",
 // 代理配置
   server: {
     port: 5173,
@@ -25,13 +26,13 @@ export default defineConfig({
       key: "./server.key",
     },
     proxy: {
-      '/api/': {
+      '/image-hosting-api/': {
         // 代理到前端的地址的代理
-        target: "http://127.0.0.1:8090",
-        // target: "https://temp-img.chrelyonly.cn/api",
+        // target: "http://127.0.0.1:8090",
+        target: "https://nginx-2.frp.chrelyonly.cn/image-hosting-api",
         // 是否跨域
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        rewrite: (path) => path.replace(/^\/image-hosting-api/, '')
       }
     }
   },
